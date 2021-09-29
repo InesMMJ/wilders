5c06dcc9093cbdf0336236390a8471dfb9e57336
InesMMJ <inesmmj97@gmail.com>
2021-09-29 12:02:47 +0100

    adding to gitHub

---
+++ wildernames.js
@@ -0,0 +1,6 @@
+const chalk = require("chalk");
+
+console.log(chalk.blue("Diogo"));
+console.log(chalk.magenta("Vasquez"));
+console.log(chalk.green("KingCUONG"));
+console.log(chalk.yellow("Natalie"));
